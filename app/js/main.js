1d1f5eb54f10b253301ce0c0bdc39cd9be33c6d0
webLashchuk$ git config --global user.name webLashchukgit config --global user.name webLashchuk <chernyadyeviv@gmail.com>
2023-03-24 23:43:32 +0200

    added resposive design

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,86 @@
 $(function () {
+    const headerHeight = $('.header').outerHeight();
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= headerHeight) {
+            $('.header').addClass('header__sticky')
+        } else {
+            $('.header').removeClass('header__sticky')
+        }
+    });
+
+    $('.menu__link, .button').on('click', function (event) {
+        event.preventDefault();
+        let scrollAnchor = $(this).attr('href')
+        let scrollPoint = $(scrollAnchor).offset().top;
+
+        if (scrollAnchor === '#catalog') {
+            scrollPoint = scrollPoint - 95;
+        }
+
+        else if (scrollAnchor === '#delivery') {
+            scrollPoint = scrollPoint - 80;
+        }
+
+        else {
+            scrollPoint = scrollPoint - 20;
+        }
+
+        $('body, html').animate({ scrollTop: scrollPoint }, 1000);
+    });
+
+    $('.burger').on('click', function () {
+        $('.mobile-menu__wrapper').toggleClass('mobile-menu__wrapper--active');
+        $('.overlay').toggleClass('overlay--active');
+        $('body').toggleClass('lock');
+    });
+
+    new Swiper('.features__inner', {
+        pagination: {
+            el: '.features__pagination',
+            clickable: true,
+            bulletClass: 'features__bullet',
+            bulletActiveClass: 'features__bullet--active',
+        },
+        autoHeight: true,
+        slidesPerGroup: 4,
+        slidesPerView: 4,
+        spaceBetween: 20,
+        breakpoints: {
+            1200: {
+                slidesPerGroup: 4,
+            },
+            992: {
+                slidesPerView: 4,
+            },
+            560: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+            },
+            320: {
+                slidesPerGroup: 1,
+                slidesPerView: 1,
+            }
+        }
+    });
+
     document.querySelectorAll(".slider").forEach((el) => {
         const thumbs = new Swiper(el.querySelector(".slider__thumbs"), {
-            slidesPerView: 4,
-            spaceBetween: 4,
             freeMode: true,
-            mousewheel: true,
+            slidesPerView: 4,
+            navigation: {
+                nextEl: ".slider__next",
+                prevEl: ".slider__prev",
+            },
             breakpoints: {
                 0: {
                     direction: "horizontal",
+                    spaceBetween: 0,
                 },
-                768: {
+                992: {
                     direction: "vertical",
+                    spaceBetween: 4,
+                    mousewheel: true,
                 },
             },
         });
@@ -19,21 +89,22 @@ $(function () {
             direction: "vertical",
             slidesPerView: 1,
             spaceBetween: 32,
-            mousewheel: true,
+            navigation: {
+                nextEl: ".slider__img-next",
+                prevEl: ".slider__img-prev",
+            },
             grabCursor: true,
             thumbs: {
                 swiper: thumbs,
             },
-            navigation: {
-                nextEl: ".slider__next",
-                prevEl: ".slider__prev",
-            },
             breakpoints: {
                 0: {
                     direction: "horizontal",
+                    mousewheel: false,
                 },
-                768: {
+                992: {
                     direction: "vertical",
+                    mousewheel: true,
                 },
             },
         });
@@ -44,6 +115,32 @@ $(function () {
         // options
         itemSelector: '.reviews__item',
         fitWidth: true,
-        columnWidth: 580,
     });
+
+    new Swiper('.reviews__wrapper', {
+        pagination: {
+            el: '.reviews__pagination',
+            clickable: true,
+            bulletClass: 'reviews__bullet',
+            bulletActiveClass: 'reviews__bullet--active',
+        },
+        autoHeight: true,
+        spaceBetween: 20,
+        breakpoints: {
+            560: {
+                slidesPerView: 1.7,
+                centeredSlides: true,
+                centeredSlidesBounds: true,
+                spaceBetween: 40,
+            },
+            320: {
+                slidesPerGroup: 1,
+                slidesPerView: 1,
+                centeredSlides: true,
+                centeredSlidesBounds: true
+            }
+        }
+    });
+
+    
 });
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,43 +1,146 @@
 $(function () {
+    const headerHeight = $('.header').outerHeight();
 
-    const sliderThumbs = new Swiper('.slider__thumbs', { 
-        // задаем параметры
-        direction: 'vertical',
-        slidesPerView: 4, 
-        spaceBetween: 4, 
-        navigation: { 
-            nextEl: '.slider__next', 
-            prevEl: '.slider__prev' 
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= headerHeight) {
+            $('.header').addClass('header__sticky')
+        } else {
+            $('.header').removeClass('header__sticky')
+        }
+    });
+
+    $('.menu__link, .button').on('click', function (event) {
+        event.preventDefault();
+        let scrollAnchor = $(this).attr('href')
+        let scrollPoint = $(scrollAnchor).offset().top;
+
+        if (scrollAnchor === '#catalog') {
+            scrollPoint = scrollPoint - 95;
+        }
+
+        else if (scrollAnchor === '#delivery') {
+            scrollPoint = scrollPoint - 80;
+        }
+
+        else {
+            scrollPoint = scrollPoint - 20;
+        }
+
+        $('body, html').animate({ scrollTop: scrollPoint }, 1000);
+    });
+
+    $('.burger').on('click', function () {
+        $('.mobile-menu__wrapper').toggleClass('mobile-menu__wrapper--active');
+        $('.overlay').toggleClass('overlay--active');
+        $('body').toggleClass('lock');
+    });
+
+    new Swiper('.features__inner', {
+        pagination: {
+            el: '.features__pagination',
+            clickable: true,
+            bulletClass: 'features__bullet',
+            bulletActiveClass: 'features__bullet--active',
         },
-        freeMode: true, 
-        mousewheel: true,
-        breakpoints: { 
-            0: { 
-                direction: 'horizontal',
+        autoHeight: true,
+        slidesPerGroup: 4,
+        slidesPerView: 4,
+        spaceBetween: 20,
+        breakpoints: {
+            1200: {
+                slidesPerGroup: 4,
             },
-            768: { 
-                direction: 'vertical', 
+            992: {
+                slidesPerView: 4,
+            },
+            560: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+            },
+            320: {
+                slidesPerGroup: 1,
+                slidesPerView: 1,
             }
         }
     });
-    // Инициализация слайдера изображений
-    const sliderImages = new Swiper('.slider__images', { 
-        // задаем параметры
-        direction: 'vertical', 
-        slidesPerView: 1, 
-        spaceBetween: 32, 
-        mousewheel: true,
-        grabCursor: true, 
-        thumbs: { 
-            swiper: sliderThumbs 
-        },
-        breakpoints: { 
-            0: { 
-                direction: 'horizontal', 
+
+    document.querySelectorAll(".slider").forEach((el) => {
+        const thumbs = new Swiper(el.querySelector(".slider__thumbs"), {
+            freeMode: true,
+            slidesPerView: 4,
+            navigation: {
+                nextEl: ".slider__next",
+                prevEl: ".slider__prev",
             },
-            768: { 
-                direction: 'vertical', 
+            breakpoints: {
+                0: {
+                    direction: "horizontal",
+                    spaceBetween: 0,
+                },
+                992: {
+                    direction: "vertical",
+                    spaceBetween: 4,
+                    mousewheel: true,
+                },
+            },
+        });
+
+        const slider = new Swiper(el.querySelector(".slider__images"), {
+            direction: "vertical",
+            slidesPerView: 1,
+            spaceBetween: 32,
+            navigation: {
+                nextEl: ".slider__img-next",
+                prevEl: ".slider__img-prev",
+            },
+            grabCursor: true,
+            thumbs: {
+                swiper: thumbs,
+            },
+            breakpoints: {
+                0: {
+                    direction: "horizontal",
+                    mousewheel: false,
+                },
+                992: {
+                    direction: "vertical",
+                    mousewheel: true,
+                },
+            },
+        });
+    });
+
+    var elem = document.querySelector('.reviews__list');
+    var msnry = new Masonry(elem, {
+        // options
+        itemSelector: '.reviews__item',
+        fitWidth: true,
+    });
+
+    new Swiper('.reviews__wrapper', {
+        pagination: {
+            el: '.reviews__pagination',
+            clickable: true,
+            bulletClass: 'reviews__bullet',
+            bulletActiveClass: 'reviews__bullet--active',
+        },
+        autoHeight: true,
+        spaceBetween: 20,
+        breakpoints: {
+            560: {
+                slidesPerView: 1.7,
+                centeredSlides: true,
+                centeredSlidesBounds: true,
+                spaceBetween: 40,
+            },
+            320: {
+                slidesPerGroup: 1,
+                slidesPerView: 1,
+                centeredSlides: true,
+                centeredSlidesBounds: true
             }
         }
     });
-})
+
+    
+});
